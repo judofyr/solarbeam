d601c46a6cd792e070e34b796b422ffaefa3ff0c
Magnus Holm <judofyr@gmail.com>
2011-08-16 13:24:01 +0200

    Abstract away parameter handling

--- lib/SolarBeam.pm
+++ lib/SolarBeam.pm
@@ -10,44 +10,71 @@ has 'url';
 has 'mojo_url' => sub { Mojo::URL->new(shift->url) };
 has 'user_agent' => sub { Mojo::UserAgent->new };
 
+my $escape_all   = quotemeta( '+-&|!(){}[]^"~*?:\\' );
+my $escape_wilds = quotemeta( '+-&|!(){}[]^"~:\\' );
+
 sub search {
   my $callback = pop;
   my ($self, $query, %options) = @_;
+  my $options = \%options;
   $query = $self->build_query($query);
 
   my $url = $self->mojo_url->clone;
   $url->path('select');
   $url->query(q => $query, wt => 'json');
 
-  my $page = $options{page};
-  if ($page) {
-    die "You must provide both page and rows" unless $options{rows};
-    $options{start} = ($page - 1) * $options{rows};
-    delete $options{page};
-  }
+  my $page = $options->{page};
+  $self->handle_page($options->{page}, $options) if $page;
 
-  $url->query(\%options);
+  $url->query($options);
 
   $self->user_agent->get($url, sub {
     my $res = SolarBeam::Response->new(pop->res->json);
 
     if ($page && $res->ok) {
       $res->pager->current_page($page);
-      $res->pager->entries_per_page($options{rows});
+      $res->pager->entries_per_page($options->{rows});
     }
 
     $callback->(shift, $res);
   });
 }
 
+sub handle_page {
+  my ($self, $page, $options) = @_;
+  die "You must provide both page and rows" unless $options->{rows};
+  $options->{start} = ($page - 1) * $options->{rows};
+  delete $options->{page};
+}
+
 sub build_query {
   my ($self, $query) = @_;
 
-  if (ref $query) {
+  my $type = ref($query);
+  if ($type eq 'HASH') {
     SolarBeam::Query->new($query);
+  } elsif ($type eq 'ARRAY') {
+    my ($raw, %params) = @{$query};
+    $raw =~ s|%([a-z]+)|$self->escape($params{$1})|ge;
+    $raw;
   } else {
     $query;
   }
+}
+
+sub escape {
+  my $text = pop;
+  my $chars;
+
+  if (ref($text)) {
+    $text = ${$text};
+    $chars = $escape_all;
+  } else {
+    $chars = $escape_wilds;
+  }
+
+  $text =~ s{([$chars])}{\\$1}g;
+  return $text;
 }
 
 "It's super effective!";
